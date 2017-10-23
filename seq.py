c932c88dccee24d9dcf49acd405c2fe10ace38bc
admbarre <barrera.adrian.m@gmail.com>
2017-10-23 12:30:26 -0700

    Reorganization, test code.

--- seq.py
+++ seq.py
@@ -1,17 +1,6 @@
 #!/usr/local/bin/python3
 import re, sys
 
-if len(sys.argv) < 2:
-    print('Usage: seq file')
-    sys.exit()
-else:
-    filename = sys.argv[1]
-
-with open(filename) as f:
-    lines = f.readlines()
-sequence = ''.join(lines)
-print(f'Sequence: {sequence}')
-
 def permute_single_base(mutation,sequence):
     permutations = []
     seq = list(sequence)
@@ -21,21 +10,36 @@ def permute_single_base(mutation,sequence):
         permutations.append(''.join(copy_seq))
     return permutations
 
-test_permutations = permute_single_base('[gatc]', 'gaattc')
-# Sequence: GAATTC
-permutations  = [
-        '[gatc]aattc',
-        'g[gatc]attc',
-        'ga[gatc]ttc',
-        'gaa[gatc]tc',
-        'gaat[gatc]c',
-        'gaatt[gatc]']
-
-print(f'Test: {test_permutations}')
-print(f'Actual: {permutations}')
-print(f'Test == Actual: {test_permutations == permutations}')
+def test(test, actual):
+    print(f'Test: {test}')
+    print(f'Actual: {actual}')
+    print(f'Test == Actual: {test == actual}')
 
 def main():
+    # Commandline argument parsing
+    if len(sys.argv) < 2:
+        print('Usage: seq file')
+        sys.exit()
+    else:
+        filename = sys.argv[1]
+
+    # Load file
+    with open(filename) as f:
+        lines = f.readlines()
+    sequence = ''.join(lines)
+
+    # Testing
+    permutations = permute_single_base('[gatc]', 'gaattc')
+    # Sequence: GAATTC
+    test_permutations  = [
+            '[gatc]aattc',
+            'g[gatc]attc',
+            'ga[gatc]ttc',
+            'gaa[gatc]tc',
+            'gaat[gatc]c',
+            'gaatt[gatc]']
+    test(permutations, test_permutations)
+
     mutations = []
     for p in permutations:
         matches = re.findall(p,sequence)
